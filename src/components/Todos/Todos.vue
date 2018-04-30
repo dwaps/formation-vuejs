abbe41760793a0bde8980d553f8ef7765c8d8728
Michael Cornillon <contact@dwaps.fr>
2018-04-30 10:47:01 +0200

    affichage d'un todo test et binding d'un new todo

--- src/components/Todos/Todos.vue
+++ src/components/Todos/Todos.vue
@@ -2,12 +2,29 @@
   <section class="todoapp">
     <header class="header">
       <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo">
     </header>
+    <div class="main">
+      <ul class="todo-list" v-for="todo in todos">
+        <li class="todo">
+          <div class="view"><label>{{ todo.name }}</label></div>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      todos: [{
+        name: 'Tache de test',
+        completed: false
+      }],
+      newTodo: '',
+    }
+  }
 }
 </script>
 
